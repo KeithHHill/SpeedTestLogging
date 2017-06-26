8637ec7b5ad8d4cbd59633ce6d631bda88a32137
keith <diknak@gmail.com>
2017-06-25 23:30:02 -0400

    (optional) if under thresshold, script will continue to run to measure how long it was under the threshhold

--- SpeedTestLogging/SpeedTestLogging/ExecuteTest.py
+++ SpeedTestLogging/SpeedTestLogging/ExecuteTest.py
@@ -8,10 +8,45 @@
 
 import speedtest
 import SpeedTestLogging
+import os
+import ConfigParser
+import sys
+import time
 
 results = speedtest.SpeedtestResults
 
 Logging = SpeedTestLogging
+myPath = os.path.dirname(os.path.abspath(__file__))
+
+# get config info
+try :
+        config = ConfigParser.ConfigParser()
+        config.read(myPath + "/config.ini")
+        maxSize = int(config.get('file','size')) * 1000  #size in bytes
+        loggingEnabled = config.get('options','loggingEnabled').lower()
+        emailEnabled = config.get('options','emailEnabled').lower()
+        minDownload = int(config.get('options','mindownload'))
+        minUpload = int(config.get('options','minupload'))
+        rerunInterval = int(config.get('options','rerunInterval'))
+        extendedLoggingEnabled = config.get('options','extendedLoggingEnabled').lower()
+        maxConsecutive = int(config.get('options','maxConsecutive'))
+
+except : 
+        print ("error accessing the config file.  Ensure file titled 'config.ini' exists.")
+        raise
+
+try :
+        version = config.get('options','version')
+except :
+        print ("unable to find version number in config file.  Ensure version is 2")
+        raise
+
+# check config version is 2
+if version != "2" :
+    print("Incorrect version nmber in the config file.  Expecting version 2")
+    os.system("pause")
+    sys.exit()
+    
 
 
 #start by running the upload and download test
@@ -19,8 +54,38 @@ results = Logging.executeTest()
 
 
 #log the results into a local file
-Logging.logResults (results)
+if loggingEnabled == "true" :
+    Logging.logResults (results, "standard")
 
 
 #email the results if below a certain threshhold
-Logging.emailResults(results)
+if ((results.download < minDownload or results.upload < minUpload) and emailEnabled) == "true" :
+    Logging.emailResults(results)
+
+
+#if the results are below the threshhold, continue to run and log
+if ((results.download < minDownload or results.upload < minUpload) and extendedLoggingEnabled) == "true" :
+    print("Results below threshhold. Beginning extended logging")
+    count = 0
+    done = False
+
+
+    while not done :
+        # perform the test again
+        results = Logging.executeTest()
+        
+        # log the results
+        Logging.logResults (results, "extended")
+
+        # increment and see if we should break the loop
+        count += 1
+        if (count >= maxConsecutive or (results.download >= minDownload and results.upload >= minUpload)) :
+            done = True
+            print("Results exceeded threshhold or consecutive max has been hit")
+        else :
+            print("Results continue to be under threshhold.  Waiting for " + str(rerunInterval) + " seconds")
+            time.sleep(30)
+            
+
+
+    
--- SpeedTestLogging/SpeedTestLogging/SpeedTestLogging.py
+++ SpeedTestLogging/SpeedTestLogging/SpeedTestLogging.py
@@ -66,24 +66,39 @@ def executeTest ( ) :
     return (s.results)
 
 
-def logResults (results) :
-    
+def logResults (results, mode) :
+   # Results are a passed dataset of speed test results.  
+   # Mode indicates if the logging is through the standard or extended logging.
+   
     try :
         config = ConfigParser.ConfigParser()
         config.read(myPath + "/config.ini")
         maxSize = int(config.get('file','size')) * 1000  #size in bytes
     except : 
-        print ("error accessing the log file.  Ensure file titled 'config.ini' exists.")
+        print ("error accessing the config file.  Ensure file titled 'config.ini' exists.")
         raise
+
+
+    if mode == "standard" :
+        logDirectory = "/logging"
+        archiveDirectory = logDirectory+ "/archive"
+        logFile = myPath + logDirectory + "/current_log.csv"
+    elif mode == "extended" :
+        logDirectory = "/logging"
+        archiveDirectory = logDirectory+ "/archiveExtended"
+        logFile = myPath + logDirectory + "/extended_log.csv"
+        
+
+
+    
     
     
     #make sure the logging folder exists
-    if not(os.path.isdir(myPath + "/logging/")) :
-        os.makedirs(myPath + "/logging/")
+    if not(os.path.isdir(myPath + logDirectory)) :
+        os.makedirs(myPath + logDirectory)
 
 
-    logFile = myPath +"/logging/current_log.csv"
-
+    
     # determine if a file exists.  If not, create it.
     if os.path.isfile(logFile) :
         print ("accessing log file")
@@ -99,11 +114,11 @@ def logResults (results) :
         print("file too large. Archiving")
          
         #check and make sure an archive folder exists
-        if not(os.path.isdir(myPath + "/logging/archive")) :
-            os.makedirs(myPath + "/logging/archive")
+        if not(os.path.isdir(myPath + archiveDirectory)) :
+            os.makedirs(myPath + archiveDirectory)
 
         name = str(datetime.datetime.now()).replace('-','').replace(':','').replace('.','').replace(' ','')+".csv"
-        archiveFile = myPath + "/logging/archive/"+ name
+        archiveFile = myPath + archiveDirectory + name
         os.rename(logFile,archiveFile)
         heading = "serverID,server name,location,datetime,?,ping,download,upload"
         open(logFile, 'ab').write(heading)
@@ -128,9 +143,8 @@ def emailResults(results) :
     try :
         config.read(myPath + "/config.ini")
     
-        minDownload = int(config.get('email','mindownload'))
-        minUpload = int(config.get('email','minupload'))
-        enabled = config.get('email','enabled')
+        minDownload = int(config.get('options','mindownload'))
+        minUpload = int(config.get('options','minupload'))
         fromaddr = config.get('email','fromaddr')
         toaddr = config.get('email','toaddr')
         svr = config.get('email','svr')
@@ -143,30 +157,27 @@ def emailResults(results) :
         print ("error accessing the config file.  Ensure a file named 'config.ini' exists \n")
         raise
 
-    if (enabled == "true") :  #must be enabled in the config file
-
-        if (results.download < minDownload or results.upload < minUpload) :
-            print("results below the threshold, sending notification")
-        
-            # compose the message
-            msg = "\r\n".join([
-                "From: " + fromaddr,
-                "To: " + toaddr,
-                "Subject: " + subject,
-                  "",
-                  message + " \n download = " + str(round(results.download,2)) + " \n upload = " + str(round(results.upload,2))
-                  ])
-        
-            # send the email
-            server = smtplib.SMTP(svr)
-            server.ehlo()
-            server.starttls()
-            server.login(euser,epasswd)
-            server.sendmail(fromaddr, toaddr, msg)
-            server.quit()
     
-    else :
-        print("email function is disbaled in the config file")
+ 
+    print("results below the threshold, sending notification")
+        
+    # compose the message
+    msg = "\r\n".join([
+        "From: " + fromaddr,
+        "To: " + toaddr,
+        "Subject: " + subject,
+            "",
+            message + " \n download = " + str(round(results.download,2)) + " \n upload = " + str(round(results.upload,2))
+            ])
+        
+    # send the email
+    server = smtplib.SMTP(svr)
+    server.ehlo()
+    server.starttls()
+    server.login(euser,epasswd)
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit()
+    
 
 
     
